d230c0c69e39a81f61a22a45e3dd9fd35b3552a1
rsinghsw20 <rsinghsw20@gmail.com>
2026-01-18 22:56:00 +0530

    hello again

--- src/data-analysis/panda-learning.py
+++ src/data-analysis/panda-learning.py
@@ -1,3 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+exponent = num1 ** num2
 
-print((12+12)/2)
+print("sum:", sum)
+print("product:",product)
+print("exponent:", exponent)
