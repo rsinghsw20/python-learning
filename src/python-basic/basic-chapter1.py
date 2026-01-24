f1f9c684c9f4ca258f95f42f6fe5eb1ba2152155
rsinghsw20 <rsinghsw20@gmail.com>
2026-01-24 22:13:28 +0530

    Update basic-chapter1.py

--- src/python-basic/basic-chapter1.py
+++ src/python-basic/basic-chapter1.py
@@ -24,5 +24,68 @@ print(movies)
 movies.insert(5, 1983)
 print(movies)
 fav_movies = ["The Holy Grail","The Life of Brian","The Meaning of Life"]
-for each_item in fav_movies:
+for each_flick in fav_movies:
+    print(each_flick)
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman",
+           ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+print(movies[4][0])
+for each_item in movies:
     print(each_item)
+if movies[3]>90:
+    print("Long movie")
+else:
+    print("Short movie")
+    for each_item in movies:
+        print(each_item)
+    names = ['Michael', 'Terry']
+    isinstance(names, list)
+    print(names)
+
+    names = ['Michael', 'Terry']
+    isinstance(names, list)
+    print(movies[-1])
+    if movies[3]<90:
+        print("Long movie")
+    else:
+        print("Short movie")
+print(len(movies))
+print(type(movies))
+movies.append("TBBT")
+print(movies)
+movies.pop()
+print(movies)
+for each_item in movies:
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            print(nested_item)
+    else:
+        print(each_item)
+for each_item in movies:
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            if isinstance(nested_item, list):
+                for deeper_item in nested_item:
+                    print(deeper_item)
+            else:
+                print(nested_item)
+    else:
+        print(each_item)
+def print_movies(movies):
+    for each_item in movies:
+        if isinstance(each_item, list):
+            print_movies(each_item)
+        else:
+            print(each_item)
+
+
+
+
+
+
+
+
+
+
+
+
